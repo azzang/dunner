ffdd479b5fabd7b563690ae7de89b414eeb41b04
Jasim <jasimtaqi@gmail.com>
2016-11-25 21:44:21 -0800

    ignore build bash script

--- cleanup.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cp starter-recipes.json ../heroku
-cp -r api ../heroku
-cp -r models ../heroku
-cp -r src/sounds ../heroku/public
-# cp -r bower_components/bootstrap/dist/fonts ../heroku/public
-cp -r src/images ../heroku/public
-rm ../heroku/public/main.js ../heroku/public/main.css
